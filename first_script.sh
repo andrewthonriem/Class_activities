7c91c7136764d9bfeda5de6993781eb1cc4c07f1
andrewthonriem <a.alier@alustudent.com>
2025-05-21 02:10:19 -0700

    activity

---
+++ first_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Name=Diana
+echo "$Name"
+
+math=$((2+1))
+echo "$math"
+
+name="Andrew"
+echo "Hello, $name!"
+# Hi Dear
+echo "What is your name?"
+read name
+echo "Hello, $name! Welcome to shell scripting."
